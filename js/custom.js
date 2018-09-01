24b1b5e61f3a163b39e62c49608492a54fc9b99a
talk2srj <suraj.richierich.sah3@gmail.com>
2018-09-01 11:24:23 +0530

    new page

--- js/custom.js
+++ js/custom.js
@@ -1,77 +1,81 @@
 (function($) {
-
-    "use strict";
 	
-    $(document).ready(function() {
-		
-		// PRELOADER
-        $("body").toggleClass("loaded");
-        setTimeout(function() {
-            $("body").addClass("loaded");
-        }, 3000);
-		
-		// PORTFOLIO DIRECTION AWARE HOVER EFFECT
-		var item = $("#bl-work-items>div");
-		var elementsLength = item.length;
-		for (var i = 0; i < elementsLength; i++) {
-			$(item[i]).hoverdir();
-		}
-		
-		// TEXT ROTATOR
-		$("#selector").animatedHeadline({
-             animationType: "clip"
-        });
-		
-		// BOX LAYOUT
-        Boxlayout.init();
-		
-		// REMOVE # FROM URL
-		$("a[href='#']").on("click", (function(e) {
-			e.preventDefault();
-		}));
+    $(function() {
 
-        // AJAX CONTACT FORM
-        $(".contactform").on("submit", function() {
-            $(".output_message").text("Loading...");
-
-            var form = $(this);
-            $.ajax({
-                url: form.attr("action"),
-                method: form.attr("method"),
-                data: form.serialize(),
-                success: function(result) {
-                    if (result == "success") {
-                        $(".contactform").find(".output_message").addClass("success");
-                        $(".output_message").text("Message Sent!");
-                    } else {
-                        $(".contactform").find(".output_message").addClass("error");
-                        $(".output_message").text("Error Sending!");
-                    }
-                }
-            });
-
-            return false;
-        });
-
-		// MATERIAL CAROUSEL
-        $(".carousel.carousel-slider").carousel({
-            fullWidth: true,
-            indicators: true,
-        });
-		
-		// RESUME CARDS ANIMATION
-		if ($(window).width() > 800) {
-			$(".resume-list-item, .resume-card").on("click", function() {
-				$(".resume-list-item").removeClass("is-active");
-				var e = parseInt($(this).data("index"),10);
-				$("#resume-list-item-" + e).addClass("is-active");
-				var t = e + 1,
-					n = e - 1,
-					i = e - 2;
-				$(".resume-card").removeClass("front back up-front up-up-front back-back"), $(".resume-card-" + e).addClass("front"), $(".resume-card-" + t).addClass("back"), $(".resume-card-" + n).addClass("back-back"), $(".resume-card-" + i).addClass("back")
+        var documenter = function(options) {
+			var nav = jQuery('.navigation'), obj = [],
+			section = $('#documenter-content>section');
+					
+			section.each(function(i, val) {
+				var self = $(this),
+				scrollData = parseInt(self.position().top);
+				obj.push(scrollData);				
+				
 			});
+
+			var defaults = {
+				duration : 400,
+				easing : 'easeOutCirc'
+			}
+
+			var o = $.extend({}, defaults , options);
+
+			return {
+				events : function() {
+					nav.on('click', 'a', function(e) {
+
+						var self = $(this), parent = self.parent(), id = self.attr('href');
+						$.scrollTo(id, o.duration, {
+							easing : o.easing,
+							onAfter : function() {
+								parent.siblings().removeClass('current-nav').end().addClass('current-nav');
+							}
+						});
+						e.preventDefault();
+					});
+
+					$(window).on('scroll', function() {
+
+						var thisScrollData = $(this).scrollTop(), $id;
+						
+						for ( var i = 0; i < obj.length; i ++ ) {
+																						
+							if ( thisScrollData > obj[i] && thisScrollData < obj[i + 1] ) {
+								$id = section.eq(i).attr('id');
+								$(nav).children('li').removeClass('current-nav');
+								$('a[href=#' + $id +']').parent('li').addClass('current-nav');
+							}
+						}
+
+					});
+				}
+			}
+
+		};
+
+		var NAV = new documenter({
+			duration: 1100
+		});
+		NAV.events();
+
+		if ( $('.lightbox').length ) {
+
+			var $lightbox = $('.lightbox');
+
+			$lightbox.fancybox({
+				helpers: {
+					title : {
+						type : 'outside'
+					},
+					overlay : {
+						speedOut : 0
+					}
+				}
+			});		
 		}
-		
-    });
+
+	});
 
 })(jQuery);
+
+	
